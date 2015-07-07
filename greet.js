4e5b09dadbe44916d2c5a29f7ab0dc361212a963
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-07-07 11:16:22 -0700

    Add command line utility run with node greet.js 'some name'

--- greet.js
+++ greet.js
@@ -6,4 +6,7 @@ exports.goodbye = function(name) {
   return('Goodbye, ' + name + '.')
 }
 
-//console.log(process.argv[2]);
+name = process.argv[2];  /*Dewey Luu helped me figure this out by directing me to:
+http://stackoverflow.com/questions/4351521/how-to-pass-command-line-arguments-to-node-js */
+console.log(exports.hello(name));
+console.log(exports.goodbye(name));
